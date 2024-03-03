58add0a391dd7fd61b5db9eded941f7a5fbaff9c
Maasuuum <fardousalmasum09@gmail.com>
2024-03-03 17:44:31 +0600

    includes and concat

--- typeof.js
+++ typeof.js
@@ -10,4 +10,15 @@ console.log(typeof country);
 console.log(typeof age);
 console.log(typeof isIndependent);
 console.log(typeof student);
+// check array using Array.isArray
+console.log(Array.isArray(friends));
 console.log(typeof aa);
+
+// to know some specific words or numbers that is available or not in array we use array name.includes()
+console.log(friends.includes(19)); 
+console.log(friends.includes(21)); 
+
+// concat use for add together with two ore more more arrays
+const newFriends = [12, 13, 18, 20, 22];
+const allFriends = newFriends.concat(friends);
+console.log(allFriends);
