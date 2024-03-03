28859a02db7310ff1754055ca0b5b96c091d13e1
Maasuuum <fardousalmasum09@gmail.com>
2024-03-03 17:30:36 +0600

    first commit

---
+++ typeof.js
@@ -0,0 +1,13 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {id: 121, class: 11, name: 'Agun'};
+const friends = [13, 12, 18, 21, 10, 21, 22];
+function aa(num1, num2){
+    return num1 + num2;
+}
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof aa);
