e583dc4272eee6b90261a1b548336eb4744ec34a
Maasuuum <fardousalmasum09@gmail.com>
2024-03-12 19:09:02 +0600

    splic

---
+++ splice.js
@@ -0,0 +1,7 @@
+const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
+const partial = friends.splice(3, 5);
+// Removes elements from an array 
+// and, if necessary, inserts new elements in their place, 
+// returning the deleted elements.
+// will change the original array
+console.log(partial);
