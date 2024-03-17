98491244acd0d0a884930cf07f28820eaccdd544
Maasuuum <fardousalmasum09@gmail.com>
2024-03-17 09:51:18 +0600

    1 to 50

---
+++ divisible.js
@@ -0,0 +1,10 @@
+/*
+1.Show output from: 1-50
+2.If the numbers divisible by 3 then instead of the number show 'foo' 
+3.If the number is divisible by 5 then instead of the number show 'bar'
+4.If the number is divisible by both 3 and 5 then instead of the number show 'foobar'
+*/
+
+for(let i = 1; i <= 50; i++){
+    console.log(i);
+}
