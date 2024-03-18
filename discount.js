8e1d5df618d7fc82c1b2ac3f9889b8556b16c8b1
Maasuuum <fardousalmasum09@gmail.com>
2024-03-18 23:27:59 +0600

    more calculation

--- discount.js
+++ discount.js
@@ -15,6 +15,21 @@ function ticketPrice(ticketQuantity){
         const price = ticketQuantity * first100Rate;
         return price;
     }
+    else if(ticketQuantity <= 200){
+        const first100Price = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTickePrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTickePrice;
+        return totalPrice;
+    }
+    else{
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTickePrice = restTicketQuantity * restTicketRate;
+        const totalCost = first100Price + second100Price + restTickePrice;
+        return totalCost;
+    }
 }
-const price = ticketPrice(1);
+const price = ticketPrice(220);
 console.log('Price: ', price);
