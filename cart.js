44485576d322725eeeb81b66b0bea053f5d13431
Maasuuum <fardousalmasum09@gmail.com>
2024-03-18 22:40:30 +0600

    total price in cart

--- cart.js
+++ cart.js
@@ -5,10 +5,13 @@ const shoppingCart = [
     {name: 'belt', price: 500},
 ];
 function totalCost(products){
+    let sum = 0;
     for(let i = 0; i< products.length; i++){
         const product = products[i];
+        sum = sum + product.price;
         console.log(product);
     }
+    return sum;
 }
 const expense = totalCost(shoppingCart);
 console.log('Total expense today:', expense);
